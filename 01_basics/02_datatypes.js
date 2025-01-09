e077b5878dca2866fb94019828a334a7385bd08e
Devansh <82436213+devansh22002@users.noreply.github.com>
2025-01-09 16:14:43 +0000

    commit conversion Operation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //it will treat all js code as newer version
+
+// code readibility should be high
+
+let name = " Devansh" // string data type
+let age = 22 // integer
+let isLogin = false
+
+
+/* 
+Types of Data types
+
+Primitive data type
+
+integer
+boolean = true/false
+bigint
+string = ""
+number => 2 power 53
+null = standalone value
+undefined => 
+symbol => used to find uniqueness
+
+*/
+
+console.log(typeof undefined) //output undefined
+console.log(typeof null)  // object
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score = "23";
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+
+
+*/
+
+let islogedin = 1;
+let isbooleanLogedin =  Boolean(islogedin);
+console.log(isbooleanLogedin);
